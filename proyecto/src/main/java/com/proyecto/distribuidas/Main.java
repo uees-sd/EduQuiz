11dcac81b12b621ab7b018f255bf946a9b7ca2a8
joseb <jbaldac02@gmail.com>
2024-06-26 07:52:19 -0500

    Init2

---
+++ proyecto/src/main/java/com/proyecto/distribuidas/Main.java
@@ -0,0 +1,7 @@
+package com.proyecto.distribuidas;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ proyecto/src/main/java/com/proyecto/distribuidas/Moderador.java
@@ -0,0 +1,5 @@
+package com.proyecto.distribuidas;
+
+public class Moderador {
+
+}
